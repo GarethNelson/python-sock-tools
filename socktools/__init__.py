f98f67d9c23d9b41a9c86ccb817b0d4ccc00b122
Gareth Nelson <gareth@garethnelson.com>
2017-01-04 16:02:43 +0000

    Skeleton daemon

--- socktools/__init__.py
+++ socktools/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['base_sock','tcp_sock','udp_sock','wsgi_websocket_sock','json_mixin','msgtype_mixin','examples']
+__all__ = ['base_sock','tcp_sock','udp_sock','wsgi_websocket_sock','json_mixin','msgtype_mixin','examples','daemon']
